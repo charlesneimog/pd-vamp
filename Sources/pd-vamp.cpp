7cbccf77da7ecb31e87c1f330450ca0469860a52
Charles K. Neimog <charlesneimog@outlook.com>
2024-07-12 19:15:35 -0300

    Working

--- Sources/pd-vamp.cpp
+++ Sources/pd-vamp.cpp
@@ -6,12 +6,21 @@
 #include <span>
 #include <vector>
 
+#include <iostream>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
 #include <fftw3.h>
 #include <rtvamp/hostsdk.hpp>
 
 static t_class *VampObj;
 
 using namespace rtvamp::hostsdk;
+using FrequencyDomainBuffer = std::span<const std::complex<float>>; // Frequency domain buffer (FFT)
+using TimeDomainBuffer = std::span<const float>;                    // Time domain buffer
 
 // ─────────────────────────────────────
 class PdVamp {
@@ -39,15 +48,32 @@ class PdVamp {
     t_outlet *info;
 };
 
+static void GetParameterDescriptors(PdVamp *x) {
+    size_t parameterIndex = 0;
+    post("");
+    for (auto &&p : x->VampPlugin->getParameterDescriptors()) {
+        post("<== Parameter %d ==>", parameterIndex);
+        post("Identifier: %s", p.identifier.data());
+        post("Name: %s", p.name.data());
+        post("Description: %s", p.description.data());
+        if (p.unit != "") {
+            post("Unit: %s", p.unit.data());
+        } else {
+            post("Unit: N/A");
+        }
+        post("Default value: %f", p.defaultValue);
+        post("Minimum value: %f", p.minValue);
+        post("Maximum value: %f", p.maxValue);
+        post("");
+    }
+}
 // ─────────────────────────────────────
 static void LoadVampPlugin(PdVamp *x, t_symbol *s) {
     std::string Id = s->s_name;
     x->VampPlugin = loadPlugin(Id, x->Sr);
-    if (x->VampPlugin->getInputDomain() == Plugin::InputDomain::Frequency) {
-        pd_error(nullptr, "Plugin %s is time domain | not implemented yet",
-                 Id.c_str());
-    }
-    x->VampPlugin->initialise(x->BlockSize, x->BlockSize);
+    int BlockSize = x->VampPlugin->getPreferredBlockSize();
+    int StepSize = x->VampPlugin->getPreferredStepSize();
+    x->VampPlugin->initialise(x->HopSize, x->HopSize);
     x->Loaded = true;
 }
 
@@ -69,8 +95,7 @@ static void ListVampPlugins(PdVamp *x) {
 
             } catch (const std::exception &e) {
                 if (x->ReportErros) {
-                    pd_error(nullptr, "Error loading plugin %s: %s", Id.c_str(),
-                             e.what());
+                    pd_error(nullptr, "Error loading plugin %s: %s", Id.c_str(), e.what());
                 }
             }
         }
@@ -95,45 +120,41 @@ static t_int *VampAudioPerform(t_int *w) { //
         return (w + 4);
     }
 
-    std::span<const std::complex<float>>
-        bufferChannel; // Define this appropriately
-
-    const auto getInputBuffer = [&]() -> Plugin::InputBuffer {
-        if (x->VampPlugin->getInputDomain() == Plugin::InputDomain::Frequency) {
-            for (int i = 0; i < x->BlockSize; i++) {
-                x->FFTIn[i] *=
-                    0.5 * (1.0 - std::cos(2.0 * M_PI * i / (x->BlockSize - 1)));
-            }
-            fftwf_execute(x->FFTPlan);
-
-            // Convert FFTW output to std::span<const std::complex<float>>
-            return std::span<const std::complex<float>>(
-                reinterpret_cast<const std::complex<float> *>(x->FFTOut),
-                x->BlockSize);
-        } else {
-            // Assuming bufferChannel is defined elsewhere and holds the
-            // appropriate data
-            return bufferChannel;
+    if (x->VampPlugin->getInputDomain() == Plugin::InputDomain::Frequency) {
+        for (int i = 0; i < x->BlockSize; i++) {
+            x->FFTIn[i] *= 0.5 * (1.0 - std::cos(2.0 * M_PI * i / (x->BlockSize - 1)));
         }
-    };
+        fftwf_execute(x->FFTPlan);
+        FrequencyDomainBuffer FFT(reinterpret_cast<const std::complex<float> *>(x->FFTOut),
+                                  x->BlockSize);
+        auto features = x->VampPlugin->process(FFT, 0);
+        post("ok");
+    } else {
+        TimeDomainBuffer timeBuffer(x->inBuffer.data(), x->inBuffer.size());
+        auto features = x->VampPlugin->process(timeBuffer, 0);
+    }
 
     x->BlockIndex = 0;
-    auto features = x->VampPlugin->process(getInputBuffer(), 0);
     return (w + 4);
 }
 
 // ─────────────────────────────────────
-static void VampAddDsp(PdVamp *x, t_signal **sp) {
+static void PdVampAddDsp(PdVamp *x, t_signal **sp) {
     x->BlockIndex = 0;
     x->inBuffer.resize(x->HopSize, 0.0f);
 
-    // TODO: Free
-    x->FFTIn = (float *)fftwf_alloc_real(x->BlockSize);
-    x->FFTOut = (fftwf_complex *)fftwf_alloc_complex(x->BlockSize);
-    x->FFTPlan =
-        fftwf_plan_dft_r2c_1d(x->BlockSize, x->FFTIn, x->FFTOut, FFTW_MEASURE);
-
+    // TODO: Make it do not alloc twice
+    x->FFTIn = (float *)fftwf_alloc_real(x->HopSize);
+    x->FFTOut = (fftwf_complex *)fftwf_alloc_complex(x->HopSize);
+    x->FFTPlan = fftwf_plan_dft_r2c_1d(x->HopSize, x->FFTIn, x->FFTOut, FFTW_MEASURE);
     dsp_add(VampAudioPerform, 3, x, sp[0]->s_vec, sp[0]->s_n);
+}
+
+// ─────────────────────────────────────
+static void PdVampFree(PdVamp *x, t_signal **sp) {
+    fftwf_destroy_plan(x->FFTPlan);
+    fftwf_free(x->FFTIn);
+    fftwf_free(x->FFTOut);
 }
 
 // ─────────────────────────────────────
@@ -151,12 +172,11 @@ void *PdVampNew(void) {
 
 // ─────────────────────────────────────
 extern "C" void vamp_tilde_setup(void) {
-    VampObj = class_new(gensym("vamp~"), (t_newmethod)PdVampNew, nullptr,
-                        sizeof(PdVamp), CLASS_DEFAULT, A_NULL);
+    VampObj = class_new(gensym("vamp~"), (t_newmethod)PdVampNew, nullptr, sizeof(PdVamp),
+                        CLASS_DEFAULT, A_NULL);
     CLASS_MAINSIGNALIN(VampObj, PdVamp, Sample);
-    class_addmethod(VampObj, (t_method)ListVampPlugins, gensym("plugins"),
-                    A_NULL);
-    class_addmethod(VampObj, (t_method)LoadVampPlugin, gensym("load"), A_SYMBOL,
-                    A_NULL);
-    class_addmethod(VampObj, (t_method)VampAddDsp, gensym("dsp"), A_CANT, 0);
+    class_addmethod(VampObj, (t_method)ListVampPlugins, gensym("plugins"), A_NULL);
+    class_addmethod(VampObj, (t_method)LoadVampPlugin, gensym("load"), A_SYMBOL, A_NULL);
+    class_addmethod(VampObj, (t_method)GetParameterDescriptors, gensym("parameters"), A_NULL);
+    class_addmethod(VampObj, (t_method)PdVampAddDsp, gensym("dsp"), A_CANT, 0);
 }
